c55c294cd74ef9a72db0918833a76e0ead90eed5
Pallabi1811 <90077866+Pallabi1811@users.noreply.github.com>
2021-11-28 17:54:52 -0500

    cleanup

--- IS7024FinalProjectBreweryArc/BreweryArc/Pages/BrewerySingle.cshtml.cs
+++ IS7024FinalProjectBreweryArc/BreweryArc/Pages/BrewerySingle.cshtml.cs
@@ -20,22 +20,12 @@ namespace BreweriesSingle.Pages
         public void OnGetAsync(string id)
         {
 
-            InitStateDropdown();
             using (var webClient = new WebClient())
 
-
-            {
-                                
+            {               
                 {
-
-                    string json_String = webClient.DownloadString($"https://api.openbrewerydb.org/breweries/{id}") ;
+                    string json_String = webClient.DownloadString($"https://api.openbrewerydb.org/breweries/{id}") ; // Dynamically Implementing the 2nd API
                     var Brewery_details = BreweriesSingleData.BreweriesSingleCollection.FromJson(json_String);
-
-                           var abc = string.IsNullOrWhiteSpace(id);
-                           if (!abc)
-
-                    //       var Brewery_detailsList = Brewery_details.ToList();
-                    //       var IdWiseBrewerydetails = Brewery_detailsList.FindAll(x => string.Equals(x.Id.ToString(), query, StringComparison.OrdinalIgnoreCase));
 
                     if (Brewery_details.Id != null)
                     {
@@ -46,49 +36,12 @@ namespace BreweriesSingle.Pages
                     {
                         ViewData["Brewery_details"] = null;
                     }
-          //      }
-          //          else
-          //          {
-           //             ViewData["Brewery_details"] = null;
-           //         }
-
                   
                 }
                 SearchId = id;
             }
         }
-
-        private void InitStateDropdown()
-        {
-            var Count_list = new List<string>
-         {
-             { "10-56-brewing-company-knox" },
-             { "10-barrel-brewing-co-bend-1" },
-             { "10-barrel-brewing-co-bend-2" },
-             { "10-barrel-brewing-co-boise-boise" },
-             { "10-barrel-brewing-co-bend-pub-bend" },
-             { "10-barrel-brewing-co-denver-denver" },
-             { "10-barrel-brewing-co-portland" },
-             { "10-barrel-brewing-co-san-diego" },
-             {"10-torr-distilling-and-brewing-reno" },
-             {"101-brewery-quilcene" },
-             {"101-north-brewing-company-petaluma" },
-             {"105-west-brewing-co-castle-rock" },
-             {"10k-brewing-anoka" },
-             {"10th-district-brewing-company-abington" },
-             {"11-below-brewing-company-houston" },
-             {"1188-brewing-co-john-day" },
-             {"12-acres-brewing-company-killeshin" },
-             {"12-gates-brewing-company-williamsville" },
-             {"12-west-brewing-company-gilbert" },
-             {"12-west-brewing-company-production-facility-mesa" }
-
-
-        };
-
-
-            ViewData["SearchId"] = new SelectList(Count_list);
-        }
+        
     }
 }
 
