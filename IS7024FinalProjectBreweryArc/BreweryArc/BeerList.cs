e67bebfe69b7057d04092fea309fb670a2b0fc19
bhaweshJ <joshibj@mail.uc.edu>
2021-11-28 20:33:29 -0500

    Created User form

---
+++ IS7024FinalProjectBreweryArc/BreweryArc/BeerList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BreweryArc
+{
+    public class BeerList
+    {
+
+        static BeerList()
+        {
+            allBeers = new List<Beers>();
+        }
+
+        public static IList<Beers> allBeers { get; set; }
+    }
+}
---
+++ IS7024FinalProjectBreweryArc/BreweryArc/Beers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BreweryArc
+{
+    public class Beers
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FavouriteBeer { get; set; }
+        public string Capacity { get; set; }
+
+        public string Improvement { get; set; }
+
+    }
+}
